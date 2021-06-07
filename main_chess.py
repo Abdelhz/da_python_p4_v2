38c8f7069ec122e4b35fd3653fb65b3b20663001
Abdelwahid HADJ ZOUBIR <33903262+Abdelhz@users.noreply.github.com>
2021-06-07 22:30:30 +0200

    Add files via upload

--- main_chess.py
+++ main_chess.py
@@ -1,7 +1,6 @@
-import app_files.controller_chess
-from app_files.controller_chess import Controller
-
+from controller_chess import Controller
 
 controller_ch = Controller()
+
 if __name__ == "__main__":
     controller_ch.menu()
