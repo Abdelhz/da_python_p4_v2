5a137190fbf631055d8728a3bb1d338f523cb4ad
Abdelwahid HADJ ZOUBIR <33903262+Abdelhz@users.noreply.github.com>
2021-06-13 21:36:43 +0200

    Add files via upload

--- main_chess.py
+++ main_chess.py
@@ -1,4 +1,12 @@
+import os
+import sys
 from app_files.controller_chess import Controller
+
+
+main_dir = os.path.dirname(__file__)
+files_dir = os.path.join(main_dir, 'app_files')
+sys.path.append(files_dir)
+
 
 controller_ch = Controller()
 
